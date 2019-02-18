bc5363aa99334573f58215cd398a08b9f5ad004e
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2019-02-18 21:41:42 +0000

    Work done on webstie

---
+++ WEBSITE/map.js
@@ -0,0 +1,9 @@
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(55.9444,3.1870),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
