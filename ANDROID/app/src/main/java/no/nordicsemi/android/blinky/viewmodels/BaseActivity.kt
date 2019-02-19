5121bc40fc8f496f5a5ac6a5881cec3e43550c94
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2019-02-19 21:54:05 +0000

    Still working on the firebase :-(

--- ANDROID/app/src/main/java/no/nordicsemi/android/blinky/viewmodels/BaseActivity.kt
+++ ANDROID/app/src/main/java/no/nordicsemi/android/blinky/viewmodels/BaseActivity.kt
@@ -1,5 +1,6 @@
 package no.nordicsemi.android.blinky.viewmodels
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ComponentCallbacks2
 import android.content.Intent
@@ -9,6 +10,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import no.nordicsemi.android.blinky.utils.UserDatabase
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS", "DEPRECATED_IDENTITY_EQUALS")
@@ -20,10 +23,31 @@ open class BaseActivity : AppCompatActivity(), ComponentCallbacks2, TextToSpeech
     lateinit var tts: TextToSpeech //Lateinit instance of the tts
     lateinit var db: FirebaseFirestore
 
+
+    private fun addUserCollection(name: String, email: String) {
+        // Create a new user with a first and last name
+        val user = HashMap<String, Any>()
+        user["Email"] = email
+        user["Name"] = name
+        val a = arrayListOf<String>()
+        user["CollectedMarkers"] = a
+
+        // Add a new document
+        db.collection("PIR_IN").document(getDate())
+                .set(user)
+                .addOnSuccessListener { }
+                .addOnFailureListener { }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         tts = TextToSpeech(this, this)
         db = FirebaseFirestore.getInstance()
-
+        addUserCollection("", "")
+        addUserPIRIN()
+        addUserDOORCLOSED()
+        addUserDOOROPEN()
+        addUserPIROUT()
         super.onCreate(savedInstanceState)
     }
 
@@ -31,10 +55,61 @@ open class BaseActivity : AppCompatActivity(), ComponentCallbacks2, TextToSpeech
 
     }
 
+    private fun getID() {
+
+    }
+
+    private fun getTimeStamp(): String {
+        return ""
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getCurrentTimeUsingDate(): String {
+        val date = Date()
+        val strDateFormat = "hh:mm:ss a"
+        val dateFormat = SimpleDateFormat(strDateFormat)
+        return dateFormat.format(date)
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDate(): String {
+        val cal = Calendar.getInstance()
+        val format1 = SimpleDateFormat("yyyyMMdd")
+        val formatted = format1.format(cal.time)
+        println(formatted)
+        return formatted.toString()
+    }
+
+    @Synchronized
+    fun addUserPIRIN() {
+        db.collection("PIRIN").document(getDate()).collection(getTimeStamp())
+    }
+
+    @Synchronized
+    fun addUserPIROUT() {
+        // Create a new user with a first and last name
+        //  db.collection("PIR_OUT").document(getDate()).collection(getTimeStamp())
+    }
+
+    @Synchronized
+    fun addUserDOOROPEN() {
+        // Create a new user with a first and last name
+        //  db.collection("DOOR_OPEN").document(getDate()).collection(getTimeStamp())
+    }
+
+    @Synchronized
+    fun addUserDOORCLOSED() {
+        // Create a new user with a first and last name
+        //  db.collection("DOOR_CLOSED").document(getDate()).collection(getTimeStamp())
+
+    }
+
 
     /**
      * Overriding onDestroy, to safely disable the TTS
      */
+
     public override fun onDestroy() {
         try {
             tts.stop()
